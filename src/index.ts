bfbe5ef28ed38a72a3d68ae947dd1f16eb85cc66
Lucas Contini <lucaschezin@gmail.com>
2024-11-12 08:26:28 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (request: Request, response: Response) => {
+	response.status(200).send('Hello World');
+});
+
+app
+	.listen(PORT, () => {
+		console.log('Server running at PORT: ', PORT);
+	})
+	.on('error', (error) => {
+		throw new Error(error.message);
+	});
